a5eea3a97fe7a2638db66b89964807f0e2273b6e
Iapetus-11 <olimdahacker@gmail.com>
2020-06-05 13:43:38 -0400

    bot.py and stuff

--- bot.py
+++ bot.py
@@ -5,23 +5,28 @@ import asyncio
 import asyncpg
 import json
 
-logging.basicConfig(level=logging.INFO) # Should be logging.WARNING in the future, like this for debug purposes ig
-logging.getLogger("asyncio").setLevel(logging.CRITICAL) # Hide those annoying errors
+logging.basicConfig(level=logging.INFO)  # Should be logging.WARNING in the future, like this for debug purposes ig
+logging.getLogger("asyncio").setLevel(logging.CRITICAL)  # Hide those annoying errors
 
-
-with open("keys.json") as k
+with open("keys.json") as k:
     keys = json.load(k)
 
-async def get_prefix(bot, ctx):
-    return "!"
 
-bot = commands.AutoShardedBot(shard_count=1, command_prefix=get_prefix, help_command=None, case_insensitive=True, max_messages=512)
+async def get_prefix(_bot, ctx):
+    return "h!"
+
+
+bot = commands.AutoShardedBot(shard_count=1, command_prefix=get_prefix, help_command=None, case_insensitive=True,
+                              max_messages=512)
 
 # Stuffs
 bot.cmd_c = "0xFFAA00"
 
+
 async def setup_db():
-    bot.db = await asyncpg.create_pool(host="localhost", database="hypixel-bot", user="", password=keys["psql"], command_timeout=5)
+    bot.db = await asyncpg.create_pool(host="localhost", database="hypixel-bot", user="", password=keys["psql"],
+                                       command_timeout=5)
+
 
 asyncio.get_event_loop().run_until_complete(setup_db())
 
@@ -31,11 +36,13 @@ bot.cog_list = []
 for cog in bot.cog_list:
     bot.load_extension(cog)
 
+
 @bot.check
 async def _bot_check_thingy(ctx):
     if not bot.is_ready():
         await ctx.send(embed=discord.Embed(color=bot.cmd_c, description="Hold on! We're starting up!"))
         return False
     return not ctx.author.bot
+
 
 bot.run(keys["discord"], bot=True)
