930b5eff2c300200b958d759dab73a2db2b2c181
Iapetus-11 <olimdahacker@gmail.com>
2020-06-07 09:57:07 -0400

    FindGuild

---
+++ cogs/cmds/hypixel/findguild.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+import discord
+
+
+class FindGuild(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="getuserguild", aliases=["userguild"])
+    async def get_user_guild(self, ctx, user: str):
+        pass
+
+    @commands.command(name="getguild", aliases=["getguildbyname"])
+    async def get_guild(self, ctx, guild_name: str):
+        pass
+
+
+def setup(bot):
+    bot.add_cog(FindGuild(bot))
