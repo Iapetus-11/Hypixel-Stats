2c4d6d2955ccd64ec55a059107aa86a2f8dde18f
Iapetus-11 <olimdahacker@gmail.com>
2020-06-08 23:13:14 -0400

    add reload commands

--- cogs/cmds/owner.py
+++ cogs/cmds/owner.py
@@ -1,2 +1,44 @@
 import discord
 from discord.ext import commands
+
+
+class Owner(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def send(self, location, message):
+        await location.send(embed=discord.Embed(color=self.bot.cc, description=message))
+
+    @commands.command(name="unload")
+    @commands.is_owner()
+    async def unload(self, ctx, *, cog: str):
+        try:
+            self.bot.unload_extension("cogs." + cog)
+        except Exception as e:
+            await self.send(ctx, f"Error while unloading extension: {cog}\n``{e}``")
+            return
+        await self.send(ctx, f"Successfully unloaded cog: " + cog)
+
+    @commands.command(name="load")
+    @commands.is_owner()
+    async def load(self, ctx, *, cog: str):
+        try:
+            self.bot.load_extension("cogs." + cog)
+        except Exception as e:
+            await self.send(ctx, f"Error while loading extension: {cog}\n``{e}``")
+            return
+        await self.send(ctx, "Successfully loaded cog: " + cog)
+
+    @commands.command(name="reload")
+    @commands.is_owner()
+    async def reload(self, ctx, *, cog: str):
+        try:
+            self.bot.reload_extension("cogs." + cog)
+        except Exception as e:
+            await self.send(ctx, f"Error while reloading extension: {cog}\n``{e}``")
+            return
+        await self.send(ctx, "Successfully reloaded cog: " + cog)
+
+
+def setup(bot):
+    bot.add_cog(Owner(bot))
