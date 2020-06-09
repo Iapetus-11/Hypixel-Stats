027aafacb19cce2310a354530259066fc625abc9
Iapetus-11 <olimdahacker@gmail.com>
2020-06-08 23:04:45 -0400

    yeppp

--- cogs/cmds/settings.py
+++ cogs/cmds/settings.py
@@ -6,6 +6,8 @@ class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        self.db = self.bot.get_cog("Database")
+
         self.allowed_chars = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b",
                               "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
                               "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
