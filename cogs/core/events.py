09226023ab934ac642b0577f93ceeb6af22becac
Iapetus-11 <olimdahacker@gmail.com>
2020-06-08 22:31:51 -0400

    onready

--- cogs/core/events.py
+++ cogs/core/events.py
@@ -9,6 +9,11 @@ class Events(commands.Cog):
         self.db = self.bot.get_cog("Database")
 
     @commands.Cog.listener()
+    async def on_ready(self):
+        activity = discord.Activity(type=discord.ActivityType.watching, name="the stats")
+        await self.bot.change_presence(activity=activity, status=discord.Status.idle)
+
+    @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         await self.db.drop_prefix(guild.id)
 
