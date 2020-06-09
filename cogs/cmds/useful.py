5a6de14eb68e028933d7a0206609eb36a5ca8ba6
Iapetus-11 <olimdahacker@gmail.com>
2020-06-08 22:50:08 -0400

    add excellent help command

---
+++ cogs/cmds/useful.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+
+class Useful(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="ping", aliases=["pong", "ding", "dong", "shing", "shling", "schlong"]) # Checks latency between Discord API and the bot
+    async def ping(self, ctx):
+        c = ctx.message.content.lower()
+        if "pong" in c:
+            pp = "Ping"
+        elif "ping" in c:
+            pp = "Pong"
+        elif "ding" in c:
+            pp = "Dong"
+        elif "dong" in c:
+            pp = "Ding"
+        elif "shing" in c or "shling" in c:
+            pp = "Schlong"
+        elif "schlong" in c:
+            await ctx.send(embed=discord.Embed(color=self.bot.cc, description="<a:ping:692401875001278494> Magnum Dong! \uFEFF ``69.00 ms``"))
+            return
+        await ctx.send(embed=discord.Embed(color=self.bot.cc, description=f"<a:ping:692401875001278494> {pp}! \uFEFF ``{round(self.bot.latency*1000, 2)} ms``"))
+
+
+
+def setup(bot):
+    bot.add_cog(Useful(bot))
