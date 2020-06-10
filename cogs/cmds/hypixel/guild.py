84ac8b3fb040c335c68b53c6ad70bfd349e0c4b2
Iapetus-11 <olimdahacker@gmail.com>
2020-06-09 21:30:00 -0400

    fix formatting

--- cogs/cmds/hypixel/guild.py
+++ cogs/cmds/hypixel/guild.py
@@ -29,11 +29,11 @@ class Guild(commands.Cog):
         created = arrow.Arrow.fromtimestamp(g.CREATED / 1000).humanize()
 
         embed.set_author(name=guild_name)
-        embed.add_field(name="Members", value=member_count, inline=True)
+        embed.add_field(name="Members", value=member_count, inline=False)
         embed.add_field(name="Coins", value=coins, inline=True)
         embed.add_field(name="XP", value=xp, inline=True)
-        embed.add_field(name="Tag", value=tag, inline=True)
-        embed.add_field(name="Created", value=created, inline=True)
+        embed.add_field(name="Tag", value=tag, inline=False)
+        embed.add_field(name="Created", value=created, inline=False)
 
         await ctx.send(embed=embed)
 
