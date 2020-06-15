f179aade7f92420c51b32172bdacadd418c02fdf
Iapetus11 <olimdahacker@gmail.com>
2020-06-15 14:35:34 -0400

    stuffs

--- cogs/core/events.py
+++ cogs/core/events.py
@@ -21,7 +21,7 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, msg):
-        if "@Hypixel Stats" in msg.content:
+        if msg.content.startswith(msg.guild.get_member(self.bot.user.id).mention):
             if msg.guild is not None:
                 prefix = await self.db.get_prefix(msg.guild.id)
             else:
@@ -29,7 +29,7 @@ class Events(commands.Cog):
             help_embed = discord.Embed(color=self.bot.cc,
                                        description=f"The prefix for this server is ``{prefix}`` and the help command is ``{prefix}help``\n"
                                                    f"If you are in need of more help, you can join the **[Support Server](https://discord.gg/{self.bot.guild_invite_code})**.")
-            help_embed.set_author(name="Villager Bot", icon_url=str(self.bot.user.avatar_url_as(static_format="png")))
+            help_embed.set_author(name="Hypixel Stats", icon_url=str(self.bot.user.avatar_url_as(static_format="png")))
             help_embed.set_footer(text="Made by Iapetus11 & TrustedMercury!")
             try:
                 await msg.channel.send(embed=help_embed)
