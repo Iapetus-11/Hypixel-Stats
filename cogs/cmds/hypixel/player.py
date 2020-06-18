538798a6f2da4b649c493d2669018a70f1ca08a4
Iapetus-11 <olimdahacker@gmail.com>
2020-06-18 19:51:07 -0400

    remove old shit

--- cogs/cmds/hypixel/player.py
+++ cogs/cmds/hypixel/player.py
@@ -586,50 +586,6 @@ class Player(commands.Cog):
             return
 
         embed = discord.Embed(color=self.bot.cc)
-
-        embed.set_author(name=f"{discord.utils.escape_markdown(player)}'s friends ({len(player_friends)} total!)",
-                         icon_url=await self.cache.get_player_head(puuid))
-
-        body = ""
-        count = 0
-        embed_count = 0
-        for friend in player_friends:
-            try:
-                name = await self.cache.get_player_name(friend)
-            except aiopypixel.exceptions.exceptions.InvalidPlayerError:
-                name = "Unknown User"
-            body += f"{discord.utils.escape_markdown(name)}\n\n"
-            if count > 20:
-                embed.add_field(name="\uFEFF", value=body)
-                embed_count += 1
-                count = 0
-                body = ""
-            count += 1
-        if body != "":
-            embed.add_field(name="\uFEFF", value=body)
-            embed_count += 1
-
-        if len(embed) > 5095 or embed_count > 35:
-            await ctx.send(embed=discord.Embed(color=self.bot.cc,
-                                               description=f"{discord.utils.escape_markdown(player)} has too many friends to show!"))
-            return
-
-        await ctx.send(embed=embed)
-
-    @commands.command(name="friends", aliases=["pf", "pfriends", "playerfriends", "friendsof", "player_friends"])
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    async def player_friends(self, ctx, player):
-        await ctx.trigger_typing()
-
-        puuid = await self.cache.get_player_uuid(player)
-
-        player_friends = await self.cache.get_player_friends(player)
-        if not player_friends:
-            await ctx.send(embed=discord.Embed(color=self.bot.cc,
-                                               description=f"**{discord.utils.escape_markdown(player)}** doesn't have any friends! :cry:"))
-            return
-
-        embed = discord.Embed(color=self.bot.cc)
         embed.set_author(name=f"{player}'s friends ({len(player_friends)} total!)",
                          icon_url=await self.cache.get_player_head(puuid))
 
