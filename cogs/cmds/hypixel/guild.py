225bd0fd24d209e432c0c7aba9dd654dd494460c
Iapetus-11 <olimdahacker@gmail.com>
2020-06-19 00:02:07 -0400

    fix urls

--- cogs/cmds/hypixel/guild.py
+++ cogs/cmds/hypixel/guild.py
@@ -2,6 +2,7 @@ import arrow
 import discord
 from discord.ext import commands
 from math import ceil
+from urllib import parse
 
 
 class Guild(commands.Cog):
@@ -24,11 +25,11 @@ class Guild(commands.Cog):
 
         desc = g.DESCRIPTION
         if desc is None:
-            embed = discord.Embed(color=self.bot.cc, url=f"https://hypixel.net/guilds/{g.NAME}")
+            embed = discord.Embed(color=self.bot.cc, url=parse.quote(f"https://hypixel.net/guilds/{g.NAME}"))
         else:
             length = len(author) + 2
             length = length if length > 30 else 30
-            embed = discord.Embed(color=self.bot.cc, url=f"https://hypixel.net/guilds/{g.NAME}",
+            embed = discord.Embed(color=self.bot.cc, url=parse.quote(f"https://hypixel.net/guilds/{g.NAME}"),
                                   description='\n'.join(
                                       desc[i:i + length] for i in
                                       range(0, len(desc), length)))
