2cbd720086a7027170aa12798a094a58921c04c4
Iapetus-11 <olimdahacker@gmail.com>
2020-06-18 20:37:06 -0400

    more formatting shit

--- cogs/cmds/hypixel/player.py
+++ cogs/cmds/hypixel/player.py
@@ -614,7 +614,7 @@ class Player(commands.Cog):
                     embed = discord.Embed(color=self.bot.cc)
 
                 embed.set_author(
-                    name=f"Page [{page}/{max_pages}] of {player}'s friends ({len(player_friends)} total!)",
+                    name=f"[Page {page}/{max_pages}] of {player}'s friends ({len(player_friends)} total!)",
                     icon_url=head)
 
                 for i in range(0, 3, 1):
