0162202eec654d9d3119e47a3b08f780ff1f9a5f
Iapetus-11 <olimdahacker@gmail.com>
2020-06-22 13:32:22 -0400

    add title to watchdog stats embed

--- cogs/cmds/hypixel/general.py
+++ cogs/cmds/hypixel/general.py
@@ -12,7 +12,7 @@ class General(commands.Cog):
     async def watchdog(self, ctx):
         wd40 = await self.cache.get_watchdog_stats()
 
-        embed = discord.Embed(color=self.bot.cc)
+        embed = discord.Embed(color=self.bot.cc, title=":hammer: Watchdog Stats :hammer:")
 
         auto = f"Total Bans: `{wd40.get('watchdog_total')}`\n" \
                f"Today: `{wd40.get('watchdog_rollingDaily')}`\n" \
