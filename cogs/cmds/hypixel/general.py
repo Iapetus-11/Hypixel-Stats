1e2174ea515cbe3b8e6783728646b0e246c57b7b
Iapetus-11 <olimdahacker@gmail.com>
2020-06-22 13:20:26 -0400

    watchdog stats

--- cogs/cmds/hypixel/general.py
+++ cogs/cmds/hypixel/general.py
@@ -10,7 +10,21 @@ class General(commands.Cog):
 
     @commands.command(name="watchdog", aliases=["watchdogstats", "bans"])
     async def watchdog(self, ctx):
-        pass
+        wd40 = await self.cache.get_watchdog_stats()
+
+        embed = discord.Embed(color=self.bot.cc)
+
+        auto = f"Total Bans: `{wd40.get('watchdog_total')}`\n" \
+               f"Today: `{wd40.get('watchdog_rollingDaily')}`\n" \
+               f"Last Minute: `{wd40.get('watchdog_lastMinute')}`\n"
+
+        staff = f"Total Bans: `{wd40.get('staff_total')}`\n" \
+                f"Today: `{wd40.get('staff_rollingDaily')}`\n"
+
+        embed.add_field(name="Watchdog", value=auto)
+        embed.add_field(name="Staff", value=staff)
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
