5e23d4fdaea81ad9e0dcb7970a7a3afa080005c0
Iapetus-11 <olimdahacker@gmail.com>
2020-06-22 20:54:29 -0400

    fix other shit with h command

--- cogs/cmds/hypixel/player.py
+++ cogs/cmds/hypixel/player.py
@@ -80,8 +80,7 @@ class Player(commands.Cog):
 
         embed = discord.Embed(color=self.bot.cc, description=f"[`{p.UUID}`]")
 
-        linked_acc = await self.db.get_linked_account_via_uuid(p.UUID)
-        linked_acc = linked_acc[0]
+        linked_acc = (await self.db.get_linked_account_via_uuid(p.UUID))[0]
 
         online = f"{self.bot.EMOJIS['offline_status']} offline"
         if p.LAST_LOGIN is not None and p.LAST_LOGOUT is not None:
