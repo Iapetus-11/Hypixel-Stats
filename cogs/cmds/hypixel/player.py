5b18f86732f41bf5fe835f3631306739c7ac7c2e
Iapetus-11 <olimdahacker@gmail.com>
2020-06-22 21:43:46 -0400

    remove debug

--- cogs/cmds/hypixel/player.py
+++ cogs/cmds/hypixel/player.py
@@ -107,8 +107,6 @@ class Player(commands.Cog):
         else:
             last_online = "Never"
 
-        print(p.UUID)
-
         player_pfp = await self.cache.get_player_head(p.UUID)
 
         guild = p.GUILD
