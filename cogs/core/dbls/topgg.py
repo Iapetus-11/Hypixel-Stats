ac51bae2d82751ca6c2e5db4dcd3b0ebddbb0075
Iapetus11 <olimdahacker@gmail.com>
2020-06-26 10:57:02 -0400

    update webhook path

--- cogs/core/dbls/topgg.py
+++ cogs/core/dbls/topgg.py
@@ -8,7 +8,7 @@ class TopGG(commands.Cog):
         self.bot = bot
 
         self.dblpy = dbl.DBLClient(self.bot, self.bot.dbl_keys[0],
-                                   webhook_path="/dbl_webhook_hypstats",
+                                   webhook_path="/updoot_topgg",
                                    webhook_auth=self.bot.dbl_keys[1],
                                    webhook_port=5000, autopost=True)
 
