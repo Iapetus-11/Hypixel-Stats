71bbde686e98ab2908d65b96461e91dc84e32e92
Iapetus11 <olimdahacker@gmail.com>
2020-06-26 10:29:32 -0400

    update .env example

--- cogs/core/dbls/topgg.py
+++ cogs/core/dbls/topgg.py
@@ -7,7 +7,8 @@ class TopGG(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        self.dblpy = dbl.DBLClient(self.bot, self.bot.dbl_keys[0], webhook_path="/dbl_webhook_hypstats",
+        self.dblpy = dbl.DBLClient(self.bot, self.bot.dbl_keys[0],
+                                   webhook_path="/dbl_webhook_hypstats",
                                    webhook_auth=self.bot.dbl_keys[1],
                                    webhook_port=5000, autopost=True)
 
