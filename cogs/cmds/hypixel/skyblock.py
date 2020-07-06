9a56f61fe30f3b838c65cb94ed515be358582f85
Iapetus-11 <olimdahacker@gmail.com>
2020-07-06 19:45:43 -0400

    do the same for skyblock.py

--- cogs/cmds/hypixel/skyblock.py
+++ cogs/cmds/hypixel/skyblock.py
@@ -28,7 +28,7 @@ class SkyBlock(commands.Cog):
         self.cache = self.bot.get_cog("Cache")
         self.db = self.bot.get_cog("Database")
 
-        self.embed = discord.Embed(color=await self.bot.cc())
+        self.embed = discord.Embed(color=asyncio.run(self.bot.cc()))
 
     def get_nbt(self, data):
         b64 = data["inv_armor"]["data"]
