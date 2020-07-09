777fa0db01e9c491cb6b02a12de0ea709e5cd087
Iapetus-11 <olimdahacker@gmail.com>
2020-07-08 21:28:30 -0400

    replace final kills with kills / game

--- cogs/cmds/hypixel/games.py
+++ cogs/cmds/hypixel/games.py
@@ -730,8 +730,8 @@ class Games(commands.Cog):
         embed.add_field(name="Beds Broken", value=beds_broken)
         embed.add_field(name="Total Games", value=total_games)
 
-        embed.add_field(name="Avg. Final Kills\nper Game",
-                        value=round((final_kills + 0.00001) / (total_games + 0.00001)))
+        embed.add_field(name="Avg. Kills\nper Game",
+                        value=round((kills + 0.00001) / (total_games + 0.00001)))
         embed.add_field(name="Avg. Deaths\nper Game", value=round((deaths + 0.00001) / (total_games + 0.00001), 2))
         embed.add_field(name="Avg. Beds Broken\nper Game",
                         value=round((beds_broken + 0.00001) / (total_games + 0.00001), 2))
