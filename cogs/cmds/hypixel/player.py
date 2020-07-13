fa542367897013af5f36c02685b35f4043808e8b
Iapetus-11 <olimdahacker@gmail.com>
2020-07-12 21:51:13 -0400

    fix shittttttttttttttttttt

--- cogs/cmds/hypixel/player.py
+++ cogs/cmds/hypixel/player.py
@@ -424,7 +424,7 @@ class Player(commands.Cog):
         name_hist_text = "\uFEFF"
 
         for i in range(0, len(names_descending), 1):
-            name_hist_text += f"**{len(names_descending) - i + 1}.** `{names_descending[i]}`\n"
+            name_hist_text += f"**{len(names_descending) - i}.** `{names_descending[i]}`\n"
 
         embed.description = name_hist_text
 
