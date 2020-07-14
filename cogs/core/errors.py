b680fad03606ab0249b1c556d8ea11c89788488d
Iapetus-11 <olimdahacker@gmail.com>
2020-07-14 13:30:18 -0400

    reorganize error handling

--- cogs/core/errors.py
+++ cogs/core/errors.py
@@ -26,6 +26,48 @@ class Errors(commands.Cog):
         except AttributeError:
             ctx.handled = False
 
+        if isinstance(e, commands.MaxConcurrencyReached):
+            await self.send(ctx, "Hold on! You can't use multiple of that command at once!")
+            return
+
+        if isinstance(e, commands.CommandOnCooldown):
+            descs = [
+                "Didn't your parents tell you [patience is a virtue](http://www.patience-is-a-virtue.org/)? Calm down and wait another {0} seconds.",
+                "Hey, you need to wait another {0} seconds before doing that again.",
+                "Hrmmm, looks like you need to wait another {0} seconds before doing that again.",
+                "Don't you know [patience was a virtue](http://www.patience-is-a-virtue.org/)? Wait another {0} seconds."
+            ]
+
+            await self.send(ctx, choice(descs).format(round(e.retry_after, 2)))
+            return
+        else:
+            ctx.command.reset_cooldown(ctx)
+
+        if isinstance(e, commands.errors.MissingRequiredArgument):
+            await self.send(ctx, "Looks like you're forgetting to put something in!")
+            return
+
+        if isinstance(e, commands.BadArgument):
+            await self.send(ctx, "Looks like you typed something wrong, try typing it correctly the first time, idiot.")
+            return
+
+        if isinstance(e, commands.errors.NoPrivateMessage):
+            await self.send(ctx, "This command can't be used in private chat channels.")
+            return
+
+        if isinstance(e, commands.MissingPermissions):
+            await self.send(ctx, "Nice try, but you don't have the permissions to do that.")
+            return
+
+        if isinstance(e, commands.CheckAnyFailure):
+            if "MissingPermissions" in str(e.errors):  # yes I know this is jank but it works so shhhh
+                await self.send(ctx, "Nice try, but you don't have the permissions to do that.")
+                return
+
+        if isinstance(e, commands.BotMissingPermissions):
+            await self.send(ctx, "You didn't give me proper the permissions to do that!")
+            return
+
         if isinstance(e.original, RateLimitError):
             await self.send(ctx, f"Uh oh, something took way too long, try again! If this message persists, "
                                  f"please contact us on the [support server](https://discord.gg/{self.bot.guild_invite_code}), thank you!")
@@ -52,52 +94,10 @@ class Errors(commands.Cog):
                             "That user doesn't have their account linked, or doesn't exist!\nIf you'd like to link your account, do `h!link <mc_username>`")
             return
 
-        if isinstance(e, commands.errors.NoPrivateMessage):
-            await self.send(ctx, "This command can't be used in private chat channels.")
-            return
-
-        if isinstance(e, commands.MissingPermissions):
-            await self.send(ctx, "Nice try, but you don't have the permissions to do that.")
-            return
-
-        if isinstance(e, commands.CheckAnyFailure):
-            if "MissingPermissions" in str(e.errors):  # yes I know this is jank but it works so shhhh
-                await self.send(ctx, "Nice try, but you don't have the permissions to do that.")
-                return
-
-        if isinstance(e, commands.BotMissingPermissions):
-            await self.send(ctx, "You didn't give me proper the permissions to do that!")
-            return
-
         # Commands to ignore
         for _type in [commands.CommandNotFound, commands.NotOwner, commands.CheckFailure, discord.errors.Forbidden]:
             if isinstance(e, _type):
                 return
-
-        if isinstance(e, commands.MaxConcurrencyReached):
-            await self.send(ctx, "Hold on! You can't use multiple of that command at once!")
-            return
-
-        if isinstance(e, commands.CommandOnCooldown):
-            descs = [
-                "Didn't your parents tell you [patience is a virtue](http://www.patience-is-a-virtue.org/)? Calm down and wait another {0} seconds.",
-                "Hey, you need to wait another {0} seconds before doing that again.",
-                "Hrmmm, looks like you need to wait another {0} seconds before doing that again.",
-                "Don't you know [patience was a virtue](http://www.patience-is-a-virtue.org/)? Wait another {0} seconds."
-            ]
-
-            await self.send(ctx, choice(descs).format(round(e.retry_after, 2)))
-            return
-        else:
-            ctx.command.reset_cooldown(ctx)
-
-        if isinstance(e, commands.errors.MissingRequiredArgument):
-            await self.send(ctx, "Looks like you're forgetting to put something in!")
-            return
-
-        if isinstance(e, commands.BadArgument):
-            await self.send(ctx, "Looks like you typed something wrong, try typing it correctly the first time, idiot.")
-            return
 
         if "error code: 50013" in str(e):
             await self.send(ctx, "I can't do that, you idiot.")
