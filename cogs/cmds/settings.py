c1b9d5cdeb2a99018d991f51b69d845be533da5f
Iapetus-11 <olimdahacker@gmail.com>
2020-07-22 11:38:20 -0400

    update settings.py

--- cogs/cmds/settings.py
+++ cogs/cmds/settings.py
@@ -32,7 +32,7 @@ class Settings(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def config(self, ctx):
         if ctx.invoked_subcommand is None:
-            embed = discord.Embed(color=await self.bot.cc())
+            embed = discord.Embed(color=await self.bot.cc(ctx.author.id))
             embed.set_author(name="Bot Configuration", icon_url=str(self.bot.user.avatar_url_as(static_format="png")))
             embed.add_field(name="Prefix", value=f"``{ctx.prefix}config prefix <prefix>``")
             await ctx.send(embed=embed)
@@ -40,31 +40,31 @@ class Settings(commands.Cog):
     @config.command(name="prefix", aliases=["pp", "p", "commandprefix"])
     async def config_prefix(self, ctx, prefix: str = None):
         if ctx.guild is None:
-            await ctx.send(embed=discord.Embed(color=await self.bot.cc(),
+            await ctx.send(embed=discord.Embed(color=await self.bot.cc(ctx.author.id),
                                                description="This command can't be used in dms or group chats."))
             return
 
         if prefix is None:
-            await ctx.send(embed=discord.Embed(color=await self.bot.cc(),
+            await ctx.send(embed=discord.Embed(color=await self.bot.cc(ctx.author.id),
                                                description=f"The current prefix is: ``{await self.db.get_prefix(ctx.guild.id)}``"))
         else:
             for car in prefix:
                 if car.lower() not in self.allowed_chars:
                     await ctx.send(
-                        embed=discord.Embed(color=await self.bot.cc(),
+                        embed=discord.Embed(color=await self.bot.cc(ctx.author.id),
                                             description="That prefix contains invalid characters!"))
                     return
             await self.db.set_prefix(ctx.guild.id, prefix[:10])
             s = ""
             if prefix[:10] != prefix:
                 s = "\n*Also, your prefix was too long, so we shortened it!*"
-            await ctx.send(embed=discord.Embed(color=await self.bot.cc(),
+            await ctx.send(embed=discord.Embed(color=await self.bot.cc(ctx.author.id),
                                                description=f"Changed the prefix from **``{ctx.prefix}``** to **``{prefix}``**{s}"))
 
     @config.command(name="color", aliases=["changecolor"])
     async def config_color(self, ctx, color: str = None):
         if color is None:
-            embed = discord.Embed(color=await self.bot.cc(), description=f"The current embed color is:")
+            embed = discord.Embed(color=await self.bot.cc(ctx.author.id), description=f"The current embed color is:")
             embed.set_image(
                 url=f"http://singlecolorimage.com/get/{hex(await self.db.get_color(ctx.author.id)).replace('0x', '')}/400x100")
             await ctx.send(embed=embed)
@@ -72,13 +72,14 @@ class Settings(commands.Cog):
         else:
             if color.lower() in list(self.colors):
                 await self.db.set_color(ctx.author.id, self.colors.get(color.lower()))
-                embed = discord.Embed(color=await self.bot.cc(), description=f"The new embed color is {color.lower()}")
+                embed = discord.Embed(color=await self.bot.cc(ctx.author.id),
+                                      description=f"The new embed color is {color.lower()}")
                 embed.set_image(
                     url=f"http://singlecolorimage.com/get/{hex(self.colors.get(color.lower())).replace('0x', '')}/400x100")
                 await ctx.send(embed=embed)
             else:
                 valid_colors_text = "`" + "`, `".join(list(self.colors)) + "`"
-                await ctx.send(embed=discord.Embed(color=await self.bot.cc(),
+                await ctx.send(embed=discord.Embed(color=await self.bot.cc(ctx.author.id),
                                                    description=f"\"{discord.utils.escape_markdown(color)}\""
                                                                f"is not an allowed/valid color. {valid_colors_text}"))
 
