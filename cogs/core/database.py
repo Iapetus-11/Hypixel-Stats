23c11878fbe18d8939a8060e8925201297d0acb4
Iapetus-11 <olimdahacker@gmail.com>
2020-07-22 21:50:42 -0400

    decrease premium sweep time

--- cogs/core/database.py
+++ cogs/core/database.py
@@ -85,7 +85,7 @@ class Database(commands.Cog):
                         await con.execute("DELETE FROM premium WHERE uid=$1", entry[0])
                         await self.set_color(entry[0], 15844367)
 
-            for _ in range(0, 60 * 5, 1):
+            for _ in range(0, 60 * 2.5, 1):
                 await asyncio.sleep(1)
                 if self.stop_loops:
                     return
