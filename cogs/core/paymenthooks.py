b901de75b3ec8701dcd6d464b7eee940d360b6d0
Iapetus-11 <olimdahacker@gmail.com>
2020-07-24 02:08:59 -0400

    idk

--- cogs/core/paymenthooks.py
+++ cogs/core/paymenthooks.py
@@ -22,7 +22,7 @@ class DonateHooks(commands.Cog):
             if r.headers.get("authorization") == self.bot.donatebot_auth_secret:
                 jj = await r.json()
 
-                # kuINi_O9Vq = Premium Monthly Subscription product id (currently $3 USD)
+                # kuINi_O9Vq = Premium 1 Month product id (currently $3 USD)
                 if jj.get("product_id") == "kuINi_O9Vq":
                     if jj.get("buyer_id") is not None:
                         if jj.get("status") == "completed":
